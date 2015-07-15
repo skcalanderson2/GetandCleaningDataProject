05d5a839edf4049a4e7fd63f650f4add9aa0efdc
Al Anderson <al_anderson@skc.edu>
2015-07-15 10:51:56 -0600

    Added in reads for subjects.

--- run_analysis.R
+++ run_analysis.R
@@ -4,13 +4,15 @@ library(readr)
 library(plyr)
 library(dplyr)
 
-## Read in test dataset and labels
+## Read in test dataset, labels, and subject
 data_test <- read_fwf("UCI_HAR_Dataset/test/X_test.txt", fwf_empty("UCI_HAR_Dataset/test/X_test.txt"))
 labels_test <- read_csv("UCI_HAR_Dataset/test/y_test.txt")
+subjects_test <- read_csv("UCI_HAR_Dataset/test/subject_test.txt")
 
-## Read in training dataset and labels
+## Read in training dataset, labels, and subject
 x_train <- read_fwf("UCI_HAR_Dataset/train/X_train.txt", fwf_empty("UCI_HAR_Dataset/train/X_train.txt"))
 labels_train <- read_csv("UCI_HAR_Dataset/train/y_train.txt")
+subjects_train <- read_csv("UCI_HAR_Dataset/train/subject_train.txt")
 
 ## Read in activity labels
 activity_labels <- read_delim("UCI_HAR_Dataset/activity_labels.txt", delim = " ")
