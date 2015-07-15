599e3b0b6257922a24633f260668ccd31c6b19c8
Al Anderson <al_anderson@skc.edu>
2015-07-15 11:12:10 -0600

    Change HCI_HAR_Dataset directory name to HCI HAR Dataset with the spaces. This is for a future ability to download and unzip in code.

--- run_analysis.R
+++ run_analysis.R
@@ -5,17 +5,17 @@ library(plyr)
 library(dplyr)
 
 ## Read in test dataset, labels, and subject
-data_test <- read_fwf("UCI_HAR_Dataset/test/X_test.txt", fwf_empty("UCI_HAR_Dataset/test/X_test.txt"))
-labels_test <- read_csv("UCI_HAR_Dataset/test/y_test.txt")
-subjects_test <- read_csv("UCI_HAR_Dataset/test/subject_test.txt")
+data_test <- read_fwf("UCI HAR Dataset/test/X_test.txt", fwf_empty("UCI HAR Dataset/test/X_test.txt"))
+labels_test <- read_csv("UCI HAR Dataset/test/y_test.txt")
+subjects_test <- read_csv("UCI HAR Dataset/test/subject_test.txt")
 
 ## Read in training dataset, labels, and subject
-x_train <- read_fwf("UCI_HAR_Dataset/train/X_train.txt", fwf_empty("UCI_HAR_Dataset/train/X_train.txt"))
-labels_train <- read_csv("UCI_HAR_Dataset/train/y_train.txt")
-subjects_train <- read_csv("UCI_HAR_Dataset/train/subject_train.txt")
+x_train <- read_fwf("UCI HAR Dataset/train/X_train.txt", fwf_empty("UCI HAR Dataset/train/X_train.txt"))
+labels_train <- read_csv("UCI HAR Dataset/train/y_train.txt")
+subjects_train <- read_csv("UCI HAR Dataset/train/subject_train.txt")
 
 ## Read in activity labels
-activity_labels <- read_delim("UCI_HAR_Dataset/activity_labels.txt", delim = " ")
+activity_labels <- read_delim("UCI HAR Dataset/activity_labels.txt", delim = " ")
 
 ## Read in variable names, i.e. features
-variable_names <- read_delim("UCI_HAR_Dataset/features.txt", delim = " ")
+variable_names <- read_delim("UCI HAR Dataset/features.txt", delim = " ")
